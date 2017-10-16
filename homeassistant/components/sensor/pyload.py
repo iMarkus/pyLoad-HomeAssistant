a09d7e3a37345050839f27d2416f7e96e627c06a
iMarkus <iMarkus@users.noreply.github.com>
2017-10-16 15:37:09 +0200

    Create sensor / switch

---
+++ homeassistant/components/sensor/pyload.py
@@ -0,0 +1,6 @@
+"""
+Support for monitoring pyLoad download speed.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.pyload/
+"""
---
+++ homeassistant/components/switch/pyload.py
@@ -0,0 +1,6 @@
+"""
+Support for setting the pyLoad download speed limit.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.pyload/
+"""
